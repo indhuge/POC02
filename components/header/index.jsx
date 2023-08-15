5efb18a88d6acc29fcf122a3f837656bac91e6ff
Caiki <caiki2003@gmail.com>
2023-08-15 17:35:58 -0300

    iniciado Header

---
+++ components/header/index.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable jsx-a11y/alt-text */
+import Styles from "./style.module.scss"
+import Image from "next/image"
+import Link from "next/link"
+import Logo from "../../public/assets/logo.png"
+
+const Header = () => {
+    return <div className={Styles.container}>
+        <div className={Styles.logo}>
+            <Image src={Logo}/>
+        </div>
+        <div className={Styles.menu}>
+            <Link href="">HOME</Link>
+            <Link href="">PRODUTOS</Link>
+            <Link href="">CASES</Link>
+        </div>
+        <div className={Styles.botao}>
+            <button><label>ENTRE EM CONTATO</label></button>
+        </div>
+    </div>;
+};
+
+export default Header;
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,8 @@
+import Head from 'next/head'
 import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <><Component {...pageProps} /><Head><title>IndHUGE</title></Head></> 
+  
+  
 }
