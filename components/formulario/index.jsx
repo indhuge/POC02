ca17a4235d7696b90ba712c112f7048b13bbe52a
Caiki <caiki2003@gmail.com>
2023-08-17 12:45:42 -0300

    modificado formulario

--- components/formulario/index.jsx
+++ components/formulario/index.jsx
@@ -1,14 +1,14 @@
 import Styles from "./style.module.scss"
 
 const Formulario = () => {
-    return <div className={Styles.container}>
+    return <div className={Styles.container} id="formulario">
         <form>
             <h2>ENTRE EM CONTATO E TRANSFORME A SUA PRODUÇÃO</h2>
-            <input type="text" placeholder=" NOME"/>
-            <input type="email" placeholder=" EMAIL"/>
-            <input type="text" placeholder=" TELEFONE"/>
-            <input type="text" placeholder=" MENSAGEM"/>
-            <input type="submit" className="msg" placeholder=" ENVIAR"/>
+            <input type="text" placeholder=" NOME" required/>
+            <input type="email" placeholder=" EMAIL" required/>
+            <input type="text" placeholder=" TELEFONE" pattern="^\(?[1-9]{2}\)? ?(?:[2-8]|9[1-9])[0-9]{3}\-?[0-9]{4}$" required/>
+            <input type="text" className={Styles.msg} placeholder=" MENSAGEM" required/>
+            <input type="submit" placeholder=" ENVIAR"/>
         </form>
     </div>
 }
