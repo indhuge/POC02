0f81041d5adb5bf108e080e27a0c30b2f0e87845
Caiki <caiki2003@gmail.com>
2023-08-18 13:55:08 -0300

    email

--- components/formulario/index.jsx
+++ components/formulario/index.jsx
@@ -1,6 +1,11 @@
 import Styles from "./style.module.scss"
 
 const Formulario = () => {
+
+    const SendEmail = () => {
+        axios.post("api/sendEmail").then(()=>console.log("funcionou")).catch(()=>console.log("erro"));
+    }
+
     return <div className={Styles.container} id="formulario">
         <form>
             <h2>ENTRE EM CONTATO E TRANSFORME A SUA PRODUÇÃO</h2>
@@ -8,7 +13,7 @@ const Formulario = () => {
             <input type="email" placeholder=" EMAIL" required/>
             <input type="text" placeholder=" TELEFONE" pattern="^\(?[1-9]{2}\)? ?(?:[2-8]|9[1-9])[0-9]{3}\-?[0-9]{4}$" required/>
             <input type="text" className={Styles.msg} placeholder=" MENSAGEM" required/>
-            <input type="submit" placeholder=" ENVIAR"/>
+            <input type="submit" onClick={SendEmail} placeholder=" ENVIAR"/>
         </form>
     </div>
 }
