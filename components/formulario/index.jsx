5ef09520d12df82ecdb42920e195e44e09630d44
Caiki <caiki2003@gmail.com>
2023-08-19 02:48:37 -0300

    - Configurado API de envio de Emails do Formulario

--- components/formulario/index.jsx
+++ components/formulario/index.jsx
@@ -1,20 +1,42 @@
+import React, { useState } from 'react';
 import axios from "axios";
 import Styles from "./style.module.scss"
 
 const Formulario = () => {
 
-    const SendEmail = () => {
-        axios.post("api/sendEmail").then(()=>console.log("funcionou")).catch(()=>console.log("erro"));
-    }
+    const [formData, setFormData] = useState({
+        nome: '',
+        email: '',
+        telefone: '',
+        mensagem: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('/api/send-email', formData);
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     return <div className={Styles.container} id="formulario">
-        <form>
+        <form onSubmit={handleSubmit}>
             <h2>ENTRE EM CONTATO E TRANSFORME A SUA PRODUÇÃO</h2>
-            <input type="text" placeholder=" NOME" required/>
-            <input type="email" placeholder=" EMAIL" required/>
-            <input type="text" placeholder=" TELEFONE" pattern="^\(?[1-9]{2}\)? ?(?:[2-8]|9[1-9])[0-9]{3}\-?[0-9]{4}$" required/>
-            <input type="text" className={Styles.msg} placeholder=" MENSAGEM" required/>
-            <input type="submit" onClick={SendEmail} placeholder=" ENVIAR"/>
+            <input type="text" name="nome" onChange={handleChange} placeholder=" NOME" required />
+            <input type="email" name="email" onChange={handleChange} placeholder=" EMAIL" required />
+            <input type="text" name="telefone" onChange={handleChange} placeholder=" TELEFONE" pattern="^\(?[1-9]{2}\)? ?(?:[2-8]|9[1-9])[0-9]{3}\-?[0-9]{4}$" required />
+            <input type="text" name="mensagem" onChange={handleChange} className={Styles.msg} placeholder=" MENSAGEM" required />
+            <input type="submit" placeholder=" ENVIAR" />
         </form>
     </div>
 }
