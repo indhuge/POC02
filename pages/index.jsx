0dad131a846577472edcb20c8f1062411741bd98
Caiki <caiki2003@gmail.com>
2023-08-29 11:38:21 -0300

    adicionado placeholder analytics

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,6 +18,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {/*adicionar alalytics*/}
       <div className={Styles.container}>
         <Header className={Styles.header}/>
         <Banner className={Styles.banner}/>
