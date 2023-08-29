5387dd0794a3620f88cc28a4da071291c8f8af69
Caiki <caiki2003@gmail.com>
2023-08-29 11:39:05 -0300

    Adicionado comentario para indicar onde ira a tag do analytics

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,7 +18,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/*adicionar alalytics*/}
+      {/*adicionaralalytics*/}
       <div className={Styles.container}>
         <Header className={Styles.header}/>
         <Banner className={Styles.banner}/>
