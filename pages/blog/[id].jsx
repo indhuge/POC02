ef0e627fbb8c2ed3bd67faac8ed1cacdc9c693d8
Caiki <caiki2003@gmail.com>
2023-09-11 16:20:47 -0300

    teste

--- pages/blog/[id].jsx
+++ pages/blog/[id].jsx
@@ -8,22 +8,23 @@ import Header from "@/components/headerBlog";
 import Rodape from "@/components/rodapeBlogPost";
 
 export default function Page({ page }) {
-    if(page.data === undefined){ page.data = "{a}"}
+    if (page.data === undefined) { page.data = "{a}" }
+    console.log(page?.data)
     return (
         <>
             <Head>
                 <title>{page.data.meta_title}</title>
-                {isFilled.keyText(page.data.meta_description) ? (
-                    <meta name="description" content={page.data.meta_description} />
+                {isFilled.keyText(page?.data?.meta_description) ? (
+                    <meta name="description" content={page?.data?.meta_description} />
                 ) : null}
             </Head>
             <>
                 <Header
                     className={Styles.header}
-                    dados={page.data.header[0]}
+                    dados={page?.data?.header[0]}
                 />
-                <SliceZone slices={page.data.slices} components={components}/>
-                <Rodape/>
+                <SliceZone slices={page?.data?.slices} components={components} />
+                <Rodape />
             </>
         </>
     );
@@ -33,13 +34,13 @@ export async function getStaticProps({ params }) {
     const client = createClient();
     const page = await client.getByUID("blog_post", params.id);
     return {
-      props: { page },
+        props: { page },
     };
-  }
-  
-  export function getStaticPaths() {
+}
+
+export function getStaticPaths() {
     return {
-      paths: ["/blog/teste"],
-      fallback: true,
+        paths: ["/blog/teste"],
+        fallback: true,
     };
-  }
+}
