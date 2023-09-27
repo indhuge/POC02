6b2ffcef39bd7bfdeb77495a0caa8b2f9225dade
Gabriela Hensel Ciudrowski <106684514+GabiHensel@users.noreply.github.com>
2023-09-27 15:09:43 -0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import '@/styles/globals.scss'
 import { PrismicPreview } from '@prismicio/next'
 import { repositoryName } from '@/prismicio'
@@ -8,9 +7,6 @@ export default function App({ Component, pageProps }) {
 		<>
 			<Component {...pageProps}/>
 			<PrismicPreview repositoryName={repositoryName} />
-			<Head>
-				<title>IndHUGE</title>
-			</Head>
 		</>
 	)
 }
