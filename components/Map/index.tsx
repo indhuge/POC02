04306a3407a7a137f4f689cdd4f0d181257a5556
Gabriela Hensel Ciudrowski <106684514+GabiHensel@users.noreply.github.com>
2023-10-13 15:28:01 -0300

    Adicionando marcadores

--- components/Map/index.tsx
+++ components/Map/index.tsx
@@ -7,10 +7,70 @@ const Map = ()=> (
     					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   					/>
-  					<Marker position={[51.505, -0.09]}>
-    				<Popup>
-     					A pretty CSS3 popup. <br/> Easily customizable.
-    				</Popup>
+  					<Marker position={[-25.7346, -53.0585]}>
+    					<Popup>
+     						<h5>Dois Vizinhos - PR</h5>
+    					</Popup>
+  					</Marker>
+
+					  <Marker position={[-12.5587, -55.7141]}>
+    					<Popup>
+     						<h5>Sorriso - MT</h5>
+    					</Popup>
+  					</Marker>
+
+					<Marker position={[-10.1689, -48.3317]}>
+    					<Popup>
+     						<h5>Palmas - TO</h5>
+    					</Popup>
+  					</Marker>
+					
+					  <Marker position={[-25.3935, -51.4562]}>
+    					<Popup>
+     						<h5>Guarapuava - PR</h5>
+    					</Popup>
+  					</Marker>
+
+					  <Marker position={[-23.5515, -51.4614]}>
+    					<Popup>
+     						<h5>Apucarana - PR</h5>
+    					</Popup>
+  					</Marker>
+
+					  <Marker position={[-25.5307, -49.1958]}>
+    					<Popup>
+     						<h5>São José dos Pinhais - PR</h5>
+    					</Popup>
+  					</Marker>
+
+					  <Marker position={[-26.8831, -52.4]}>
+    					<Popup>
+     						<h5>Xanxerê - SC</h5>
+    					</Popup>
+  					</Marker>
+
+					  <Marker position={[-26.2511, -49.3802]}>
+    					<Popup>
+     						<h5>São Bento do Sul - SC</h5>
+    					</Popup>
+  					</Marker>
+
+					  <Marker position={[-29.1678, -51.1794]}>
+    					<Popup>
+     						<h5>Caxias do Sul - RS</h5>
+    					</Popup>
+  					</Marker>
+
+					  <Marker position={[-29.6914, -53.8008]}>
+    					<Popup>
+     						<h5>Santa Maria - RS</h5>
+    					</Popup>
+  					</Marker>
+
+					  <Marker position={[-12.2733, -38.9556]}>
+    					<Popup>
+     						<h5>Feira de Santana - BA</h5>
+    					</Popup>
   					</Marker>
 					</MapContainer>
 )
